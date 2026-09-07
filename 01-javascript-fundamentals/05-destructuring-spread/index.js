b0a5ec2eab57c8e6a6c1cc2bda84037b491137ed
SyirinAlyaNafisa <alyanafisasyirin@gmail.com>
2026-09-07 07:48:50 +0700

    kode destructuring spread

---
+++ 01-javascript-fundamentals/05-destructuring-spread/index.js
@@ -0,0 +1,51 @@
+// MATERI: DESTRUCTURING & SPREAD OPERATOR
+
+// ----- 1. Destructuring Object -----
+const produk = {
+  nama: "Laptop",
+  harga: 8000000,
+  stok: 10,
+};
+// cara lama (tanpa destructuring)
+const namaLama = produk.nama;
+const hargaLama = produk.harga;
+//cara baru (dengan destructuring) - ambil beberapa properti sekaligus
+const { nama, harga } = produk;
+console.log(nama, harga);
+
+// ----- 2. Destructuring dengan nama variabel berbeda -----
+const { nama: namaProduk, harga: hargaProduk } = produk;
+console.log(namaProduk, hargaProduk);
+
+// ----- 3. Destructruring Array -----
+const buah = ["apel", "jeruk", "mangga"];
+//cara lama
+const buahPertamaLama = buah[0];
+const buahKeduaLama = buah[1];
+//cara baru
+const [pertama, kedua, ketiga] = buah;
+console.log(pertama, kedua, ketiga);
+
+// ----- 4. Destructuring di parameter function (sering dipakai!) -----
+function infoProduk({ nama, harga }) {
+  return `${nama} seharga Rp${harga}`;
+}
+console.log(infoProduk(produk));
+
+// ----- 5. Spread Operator pada Object -----
+// spread = "membuka" isi object/array, lalu bisa dipakai buat gabung/copy
+const produkBaru = { ...produk, diskon: true };
+console.log(produkBaru);
+
+// ----- 6. Spread Operator pada Array -----
+const buahBaru = [...buah, "melon"];
+console.log(buahBaru);
+
+// gabungin 2 array menggunakan spread(menyalin&menggabungkan data)
+const sayur = ["wortel", "bayam"];
+const semuaMakananan = [...buah, ...sayur];
+console.log(semuaMakananan);
+
+// ----- 7. Update 1 properti object tanpa mengubah yang lain -----
+const produkUpdate = { ...produk, harga: 7500000 };
+console.log(produkUpdate);
