dd907768eb475102b5c4be097a5a00411f27ee8d
SyirinAlyaNafisa <alyanafisasyirin@gmail.com>
2026-09-07 09:00:03 +0700

    rename nama file

---
+++ 01-javascript-fundamentals/mini-project-produk-list/data.js
@@ -0,0 +1,9 @@
+// DATA PRODUK (data lokal, nanti di Minggu 3-4 bakal diganti pakai api )
+export const daftarProduk = [
+  { id: 1, nama: "Laptop Asus ROG", kategori: "Elektronik", harga: 15000000 },
+  { id: 2, nama: "Mouse Logitech", kategori: "Elektronik", harga: 250000 },
+  { id: 3, nama: "Keyboard Mechanical", kategori: "Elektronik", harga: 500000 },
+  { id: 4, nama: "Meja Kerja Kayu", kategori: "Furniture", harga: 1200000 },
+  { id: 5, nama: "Kursi Gaming", kategori: "Furniture", harga: 2000000 },
+  { id: 6, nama: "Lampu Belajar LED", kategori: "Elektronik", harga: 150000 },
+];
---
+++ 01-javascript-fundamentals/mini-project-produk-list/index.js
@@ -0,0 +1,28 @@
+import { daftarProduk } from "./data.js";
+// FUNCTION 1: Cari produk berdasarkan nama (keyword)
+function cariBerdasarkanNama(data, keyword) {
+  return data.filter((produk) =>
+    produk.nama.toLowerCase().includes(keyword.toLowerCase()),
+  );
+}
+// FUNCTION 2: Filter produk berdasarkan harga maksimal
+function filterBerdasarkanHarga(data, maksimal) {
+  return data.filter((produk) => produk.harga <= maksimal);
+}
+// FUNCTION 3 (bonus): Gabungin cari + filter harga sekaligus
+function cariDanFilterHarga(data, keyword, maksimal) {
+  return data
+    .filter((produk) =>
+      produk.nama.toLowerCase().includes(keyword.toLowerCase()),
+    )
+    .filter((produk) => produk.harga <= maksimal);
+}
+//TESTING
+console.log("--- Cari 'Laptop'---");
+console.log(cariBerdasarkanNama(daftarProduk, "laptop"));
+
+console.log("--- Filter harga di bawah 500000 ---");
+console.log(filterBerdasarkanHarga(daftarProduk, 500000));
+
+console.log("--- Cari 'kursi' + harga di bawah 3000000 ---");
+console.log(cariDanFilterHarga(daftarProduk, "kursi", 3000000));
