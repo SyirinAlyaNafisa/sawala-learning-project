0951da7d2d057cf7755519dffc397369b70da741
SyirinAlyaNafisa <alyanafisasyirin@gmail.com>
2026-09-07 21:57:41 +0700

    update isi file

--- 01-javascript-fundamentals/06-es-modules/math.js
+++ 01-javascript-fundamentals/06-es-modules/math.js
@@ -5,27 +5,7 @@ export function tambah(a, b) {
   return a + b;
 }
 
-export function kurang(a, b) {
-  return a - b;
-}
-
-export const PI = 3.14;
-export function luasLingkaran(radius) {
-  return PI * radius * radius;
-}
-
 // ----- Default export (cuma boleh 1 per file) -----
-export default function kalkulator(a, b, operator) {
-  switch (operator) {
-    case "+":
-      return a + b;
-    case "-":
-      return a - b;
-    case "*":
-      return a * b;
-    case "/":
-      return a / b;
-    default:
-      return "Operator tidak valid";
-  }
+export default function kali(a,b){
+  return a* b;
 }
