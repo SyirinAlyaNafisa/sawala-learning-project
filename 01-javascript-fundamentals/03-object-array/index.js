4f97c20e1f36bf28c6dfc4c4bb13ef7b5ba05ddc
SyirinAlyaNafisa <alyanafisasyirin@gmail.com>
2026-09-07 07:42:44 +0700

    object & array

--- 01-javascript-fundamentals/03-object-array/index.js
+++ 01-javascript-fundamentals/03-object-array/index.js
@@ -2,8 +2,13 @@
 
 // ---- 1. Array  dasar -----
 const buah = ["apel", "jeruk", "mangga"];
+
 console.log(buah[0]);
 console.log(buah.length);
+buah.push("pisang"); // nambah item di akhir
+console.log(buah);
+buah.pop(); // hapus item terakhir
+console.log(buah);
 
 // ----- 2. Object  dasar -----
 const produk = {
@@ -19,25 +24,7 @@ const daftarProduk = [
   { id: 1, nama: "laptop", harga: 8000000 },
   { id: 2, nama: "Mouse", harga: 150000 },
   { id: 3, nama: "Keyboard", harga: 350000 },
+  { id: 4, nama: "Monitor", harga: 550000 },
 ];
-
-// ----- 4. map() - ubah tiap item -----
-const namaProduk = daftarProduk.map((p) => p.nama);
-console.log(namaProduk);
-
-//----- 5. filter() - ambil yang sesuai syarat -----
-const produkMahal = daftarProduk.filter((p) => p.harga > 200000);
-console.log(produkMahal);
-
-//----- 6. Find() - cari satu item -----
-const cariMouse = daftarProduk.find((p) => p.nama === "Mouse");
-console.log(cariMouse);
-
-//----- 7. forEach() - loop biasa -----
-daftarProduk.forEach((p) => {
-  console.log(`${p.nama}: Rp${p.harga}`);
-});
-
-//----- 8. reduce() - gabungin jadi 1 nilai -----
-const totalHarga = daftarProduk.reduce((total, p) => total + p.harga, 0);
-console.log("Total harga :", totalHarga);
+console.log(daftarProduk);
+console.log(daftarProduk[0].nama);
